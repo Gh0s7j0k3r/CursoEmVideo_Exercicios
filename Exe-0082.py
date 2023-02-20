561b6a3833453c3b92d7326993f7c05ae0034561
Gh0s7j0k3r <miguel.moises@ymail.com>
2023-02-20 13:10:36 -0300

    Exercicio 82

--- Exe-0082.py
+++ Exe-0082.py
@@ -3,9 +3,19 @@ Depois disso, crie duas listas extras que vão conter apenas os valores pares e 
 Ao final mostre o conteudo das tres listas geradas.'''
 
 lista = []
+par = []
+impar = []
 while True:
     lista.append(int(input("Digite um numero: ")))
     resp=str(input('Deseja continuar? [S/N] '))
     if resp in 'Nn':
         break
-    
+for c in range (0, len(lista)):
+    divisao = lista[c] % 2
+    if divisao == 0:
+        par.append(lista[c])
+    else:
+        impar.append(lista[c])
+print(f'Foram digitados os numeros: {lista}')
+print(f'A lista de pares é {par}')
+print(f'A lista de pares é {impar}')
