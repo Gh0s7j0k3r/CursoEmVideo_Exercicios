229f093485ea967e97e3bfe3c0181a9a4a6e34f3
Gh0s7j0k3r <miguel.moises@ymail.com>
2023-02-20 01:11:28 -0300

    Exercicio 81 resolvido

--- Exe-0081.py
+++ Exe-0081.py
@@ -5,4 +5,14 @@ C-Se o valor 5 foi digitado e está ou não na lista. '''
 
 valores = []
 while True:
-    
+    valores.append(int(input('Digite um numero: ')))
+    resposta = str(input('Deseja continuar? S/N '))
+    if resposta in "nN":
+        break
+print(f"Foram digitados {len(valores)} numeros.")
+valores.sort(reverse=True)
+print(f"Os valores em ordem decrescente são {valores}")
+if 5 in valores:
+    print(f"O numero 5 consta na lista.")
+else:
+    print("Não foi encontrado o numero 5 na lista.")
