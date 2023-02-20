1fc6f3be575cf3e5361c6751ed2e4b186c5ab99d
Gh0s7j0k3r <miguel.moises@ymail.com>
2023-02-19 23:13:52 -0300

    Correção do exercicio 80

--- Exe-0080.py
+++ Exe-0080.py
@@ -15,7 +15,7 @@ print(f"Os valores digitados foram {valores}")'''
 lista = []
 for c in range (0, 5):
     n = int(input('Digite um valor: '))
-    if c == 0 or lista[len(lista)-1]:
+    if c == 0 or n > lista[len(lista)-1]:
         lista.append(n)
         print('Adicionado ao final da lista...')
     else:
