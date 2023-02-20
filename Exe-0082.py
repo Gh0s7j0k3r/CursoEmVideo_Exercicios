32a8acc6295518e8c1bab9f97a9c47057f28f46e
Gh0s7j0k3r <miguel.moises@ymail.com>
2023-02-20 10:33:19 -0300

    Inicio do exercicio 82

--- Exe-0082.py
+++ Exe-0082.py
@@ -2,3 +2,10 @@
 Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores impares digitados, respectivamente. 
 Ao final mostre o conteudo das tres listas geradas.'''
 
+lista = []
+while True:
+    lista.append(int(input("Digite um numero: ")))
+    resp=str(input('Deseja continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+    
